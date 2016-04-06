7e7729dffa342ff1e2d72d78b18dd547697075d9
Adam Craig and Ashley Maceli <adam@>
2016-04-06 09:34:15 -0700

    add skeleton code and visualized grid, selection dropdown menu, and marking buttons. Initialized preliminary constructors and prototypes for game play

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// BUSINESS LOGIC
+
+function Player (mark) {
+  var markType = mark;
+}
+
+function Space (location, state) {
+  var location = location;
+  var state = state;
+}
+
+function Board (spaces) {
+  var spaces = [];
+}
+
+function Game (turn, gameOver) {
+  var turn = 0;
+  var gameOver = false;
+}
+
+Player.prototype.mark = function() {
+  return Player.mark;
+}
+
+Board.prototype.find = function(row, column) {
+
+}
+
+// USER INTERFACE LOGIC
+
+$(document).ready(function() {
+
+});
