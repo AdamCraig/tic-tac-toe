5f9bf06dec81673770870548d1157363bd907888
Adam Craig and Ashley Maceli <ashley@>
2016-04-06 10:45:55 -0700

    add basic functionality to add X mark to proper spaces in board

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function Space (location, state) {
 }
 
 function Board (spaces) {
-  var spaces = [];
+  var occupiedSpaces = new Array(9);
 }
 
 function Game (turn, gameOver) {
@@ -26,8 +26,40 @@ Board.prototype.find = function(row, column) {
 
 }
 
+// game flow
+//
+// press play?
+// initialize board - (clear values of all spaces)
+// start round 1
+// {
+// get player1  input (which space to fill)
+// according to space selected, fill space with player1 mark
+//insert mark into specific index in array (not using push)
+// check for winning condition
+// switch turns
+// get player2 input (which space to fill)
+// according to space selected, fill space with player2 mark
+// check for winning condition
+// move to next round
+// }
+// loop rounds until there's a winner or the board is full
+// check for winning condition
+// if board is full and no winner, declare tie
+//
+//
+
+
+
 // USER INTERFACE LOGIC
 
 $(document).ready(function() {
+  $("form#space-choice").submit(function(event) {
+    event.preventDefault();
 
+    var inputtedSpace = $("select.new-move").val();
+    console.log(inputtedSpace);
+
+    $("#space" + inputtedSpace).text("X");
+
+  });
 });
