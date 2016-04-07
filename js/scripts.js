dbb700e33f1073efe6abf62440e35c79630e92ad
Adam Craig and Ashley Maceli <ashley@>
2016-04-07 16:48:25 -0700

    change font style and center/clean up most elements, add numbering system to boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // BUSINESS LOGIC
-
 function Player (mark, playerColor) {
   this.mark = mark;
   this.playerColor = playerColor;
@@ -73,28 +72,6 @@ Board.prototype.checkWinningConditionO = function(allMarks) {
   }
 }
 
-// game flow
-//
-// press play?
-// initialize board - (clear values of all spaces)
-// start round 1
-// {
-// get player1  input (which space to fill)
-// according to space selected, fill space with player1 mark
-// insert mark into specific index in array (not using push)
-// check for winning condition
-// switch turns
-// get player2 input (which space to fill)
-// according to space selected, fill space with player2 mark
-// check for winning condition
-// move to next round
-// }
-// loop rounds until there's a winner or the board is full
-// check for winning condition
-// if board is full and no winner, declare tie*********************
-//
-//
-
 // USER INTERFACE LOGIC
 $(document).ready(function() {
 
@@ -152,8 +129,10 @@ $(document).ready(function() {
         $(".result").fadeIn();
         $("#play-again").fadeIn();
       } else if ( (board.checkWinningConditionO(board.occupiedSpaces) ) === false && game.turn === 9) {
-        alert("TIE");
-        // Need to add same conditions as player 1?
+        setTimeout(function() { $("#markX").hide(); }, 500);
+        $(".new-move").hide();
+        $(".tie-game").fadeIn();
+        $("#play-again").fadeIn();
       }
     }
   });
